da457f146c31d9bfb1ebe8c07042d9f8ec5d2bae
Siddharth Krishna <siddharth-krishna@users.noreply.github.com>
2016-06-22 16:41:33 +0100

    adding file to parse sudoku and call solver

--- src/pysat.py
+++ src/pysat.py
@@ -353,38 +353,38 @@ def banner():
     print("c (slowly) learning CDCL algorithms (roughly 10-50x slower than plain C/C++ CDCL implementations)")
     print("c          but this is a native Python implementation. Easy to play with!")
 
-banner()
-solver = Solver()
+if __name__ == "__main__":
+    banner()
+    solver = Solver()
 
-if len(sys.argv) > 1:
-    readFile(solver, sys.argv[1])
-    solver.buildDataStructure()
-else:
-    printUsage()
-    sys.exit(1)
+    if len(sys.argv) > 1:
+        readFile(solver, sys.argv[1])
+        solver.buildDataStructure()
+    else:
+        printUsage()
+        sys.exit(1)
 
-#result = solver.solve(maxConflict = lambda s: int(luby(s._config.restartInc, s._restarts) * 32))
-result = solver.solve(maxConflicts = lambda s: int((100*(1.5**s._restarts))))
-if result == cst.lit_False:
-    print("c UNSATISFIABLE")
-elif result == cst.lit_True:
-    print("c SATISFIABLE")
-else:
-    print("c UNKNOWN")
-solver.printStats()
+    #result = solver.solve(maxConflict = lambda s: int(luby(s._config.restartInc, s._restarts) * 32))
+    result = solver.solve(maxConflicts = lambda s: int((100*(1.5**s._restarts))))
+    if result == cst.lit_False:
+        print("c UNSATISFIABLE")
+    elif result == cst.lit_True:
+        print("c SATISFIABLE")
+    else:
+        print("c UNKNOWN")
+    solver.printStats()
 
-if result == cst.lit_True and solver._config.printModel: # SAT was claimed
-    #print("v ", end="")
-    for v in range(0, len(solver._values)):
-        val = solver._values[v]
-        assert val is not cst.lit_Undef
-        solver._finalModel.append(val==cst.lit_True)
-        if val==cst.lit_False: print("-")#, end="")
-        print(str(v+1)+ " ")#, end="")
-    print("")
+    if result == cst.lit_True and solver._config.printModel: # SAT was claimed
+        #print("v ", end="")
+        for v in range(0, len(solver._values)):
+            val = solver._values[v]
+            assert val is not cst.lit_Undef
+            solver._finalModel.append(val==cst.lit_True)
+            if val==cst.lit_False: print("-")#, end="")
+            print(str(v+1)+ " ")#, end="")
+        print("")
 
-if result == cst.lit_False:
-    sys.exit(20)
-if result == cst.lit_True:
-    sys.exit(10)
-
+    if result == cst.lit_False:
+        sys.exit(20)
+    if result == cst.lit_True:
+        sys.exit(10)
